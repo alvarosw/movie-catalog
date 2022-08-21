ea8a625ecc3e295763c9aa2ab2c31c73e9755b76
Álvaro <56317536+alvarosw@users.noreply.github.com>
2022-08-21 12:15:35 -0300

    feat: initial setup

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const port = process.env.PORT || 4000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+}
+
+bootstrap().then((_) => {
+  console.debug(
+    `
+    App running at:
+    - Local: http://localhost:${port}
+  `,
+  );
+});
