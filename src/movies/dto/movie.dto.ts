449d13dbcd1a885731c9692b832db601c5604c49
Álvaro <56317536+alvarosw@users.noreply.github.com>
2022-08-23 17:09:43 -0300

    docs: add swagger doc to movie dto

--- src/movies/dto/movie.dto.ts
+++ src/movies/dto/movie.dto.ts
@@ -1,20 +1,39 @@
-import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { Movie } from '../entities/movie.entity';
 
 export class MovieDto {
-  @IsString()
+  @ApiProperty({
+    description: 'Id do filme',
+    type: Number,
+    example: 1,
+  })
   id: Movie['id'];
 
   @IsString()
   @IsNotEmpty()
   name: Movie['name'];
 
-  @IsString()
+  @ApiProperty({
+    description: 'Sinopse do Filme',
+    type: String,
+    example:
+      "Brian O'Conner é um policial que se infiltra no submundo dos rachas de rua para investigar uma série de furtos. Enquanto tenta ganhar o respeito e a confiança do líder Dom Toretto, ele corre o risco de ser desmascarado.",
+  })
   synopsis: Movie['synopsis'];
 
-  @IsString()
+  @ApiProperty({
+    description: 'Url para uma imagem do filme',
+    type: String,
+    example: 'https://s1.fileditch.ch/gKnvxwZoLmmBLSDDKub.webp',
+  })
   image: Movie['image'];
 
-  @IsBoolean()
+  @ApiProperty({
+    description:
+      'Propriedade Lançamento. Se o filme foi lançado, verdadeiro, se não, falso',
+    type: Boolean,
+    example: true,
+  })
   released: Movie['released'];
 }
