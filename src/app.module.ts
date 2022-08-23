9141c21cc31cb5d6fec269363fe51d03fddb137b
Álvaro <56317536+alvarosw@users.noreply.github.com>
2022-08-23 15:33:00 -0300

    feat: use typeorm in modules, create movies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.config.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import * as redisStore from 'cache-manager-redis-store';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+    MoviesModule,
+  ],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Movie } from 'movie.entity';
+import { Repository } from 'typeorm';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Injectable()
+export class MoviesService {
+  constructor(
+    @InjectRepository(Movie) private readonly repository: Repository<Movie>,
+  ) {}
+  create(createMovieDto: CreateMovieDto) {
+    return 'This action adds a new movie';
+  }
+
+  async findAll() {
+    return `This action returns all movies`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} movie`;
+  }
+
+  update(id: number, updateMovieDto: UpdateMovieDto) {
+    return `This action updates a #${id} movie`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} movie`;
+  }
+}
