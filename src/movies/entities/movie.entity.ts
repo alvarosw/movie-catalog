7f2561fead4707f0d878bcb5b218189c8b6e931d
Álvaro <56317536+alvarosw@users.noreply.github.com>
2022-08-23 15:34:39 -0300

    fix: move entity to the right place

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -1,1 +1,19 @@
-export class Movie {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'movies' })
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar')
+  name: string;
+
+  @Column('text')
+  synopsis: string;
+
+  @Column('varchar')
+  image: string;
+
+  @Column({ type: 'bool', default: true })
+  released: boolean;
+}
