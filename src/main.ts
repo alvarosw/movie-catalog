960a44948aeef270d051453d56ac7625fc52e4fd
Álvaro <56317536+alvarosw@users.noreply.github.com>
2022-08-23 18:05:40 -0300

    feat: route to generated OpenApi json docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Response } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +9,14 @@ async function bootstrap() {
   const port = process.env.PORT;
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  const docConfig = new DocumentBuilder()
+    .setTitle('Movies Api')
+    .setVersion('0.0.1')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, docConfig);
+  app.use('/api/docs', (_, res: Response) => res.json(swaggerDocument));
 
   await app.listen(port, () => {
     console.debug(
