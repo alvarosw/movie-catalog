753cc28d596d0d9c9b6f8feeeb4cd66ced60f473
Álvaro <56317536+alvarosw@users.noreply.github.com>
2022-08-24 21:42:06 -0300

    fix: use redis config from env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,8 +21,8 @@ import { Auth } from './common/middleware/auth.middleware';
     CacheModule.register({
       isGlobal: true,
       store: redisStore,
-      host: 'localhost',
-      port: 6379,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
     }),
     MoviesModule,
     UsersModule,
